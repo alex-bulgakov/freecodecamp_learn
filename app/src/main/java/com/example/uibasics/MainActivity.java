fa45d8e38ee8841e11c5fa9e7fb48ec6b12f50db
alebul <master8423@gmail.com>
2022-05-20 10:58:56 +0300

    add checkbox code to MainActivity.java

--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -4,14 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox checkBoxHarry, checkBoxMatrix, checkBoxJocker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkBoxHarry = findViewById(R.id.checkboxHarry);
+        checkBoxMatrix = findViewById(R.id.checkboxMatrix);
+        checkBoxJocker = findViewById(R.id.checkboxJocker);
+        
+        if (checkBoxHarry.isChecked()) {
+            Toast.makeText(this, "You watch Harry Potter", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "You need to watch Harry Potter", Toast.LENGTH_SHORT).show();
+        }
+
+        checkBoxHarry.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(MainActivity.this, "You watch Harry Potter", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "You need to watch Harry Potter", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
