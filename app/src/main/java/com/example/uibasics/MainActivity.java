8ae6d2ed871da47fcfe8ebf4587ad2bf94dc11f9
Alebul <master8423@gmail.com>
2022-05-21 07:44:30 +0300

    add logic to spinner

--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ListView citiesList;
+    private Spinner studentsSpinner;
 
 
 
@@ -26,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         citiesList = findViewById(R.id.citiesList);
+        studentsSpinner = findViewById(R.id.studentsSpinner);
+
+        ArrayList<String> students = new ArrayList<>();
+        students.add("Student John");
+        students.add("Student Alex");
+        students.add("Student Ivan");
+        students.add("Student Vovan");
+        students.add("Student Mixa");
+
         final ArrayList<String> cities = new ArrayList<>();
         cities.add("Moscow");
         cities.add("Kiev");
@@ -34,8 +45,23 @@ public class MainActivity extends AppCompatActivity {
         cities.add("London");
 
         ArrayAdapter<String> citiesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities);
+        ArrayAdapter<String> studentsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, students);
 
         citiesList.setAdapter(citiesAdapter);
+        studentsSpinner.setAdapter(studentsAdapter);
+
+
+        studentsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, students.get(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         citiesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -43,6 +69,10 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Click on " + cities.get(position), Toast.LENGTH_SHORT).show();
             }
         });
+
+
+
+
     }
 
 }
