9f784ee6ed75bef96516033b5bfe3d09ecdadc19
Alebul <master8423@gmail.com>
2022-05-21 06:39:22 +0300

    add a listview

--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -10,36 +10,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
-    private TextView txtHello;
-    private EditText edtTxtName;
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnHello:
-                Toast.makeText(this, "Hello User!!!", Toast.LENGTH_SHORT).show();
-                txtHello.setText("Hello " + edtTxtName.getText().toString());
-            default:
-                break;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btnHello = findViewById(R.id.btnHello);
-        btnHello.setOnClickListener(this);
-
-        btnHello.setOnLongClickListener(v -> {
-            Toast.makeText(MainActivity.this, "OnLongClickLiestener", Toast.LENGTH_LONG).show();
-            return true;
-        });
-
-        edtTxtName = findViewById(R.id.edtText);
-        txtHello = findViewById(R.id.txtHello);
-
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
