0b40b3785315cc813d800cd81556a79decff1499
Alebul <master8423@gmail.com>
2022-05-22 22:56:08 +0300

    add listener to float btn

--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -9,13 +9,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity  {
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    fab = findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Fab clicked", Toast.LENGTH_SHORT).show();
+        }
+    });
 
     }
 
